e4c2685810c1c42aacdd3144312f9a5481e4b132
Gonzalo Airasca <gonzaloairasca@gmail.com>
2023-04-26 19:50:55 -0300

    corrigiendo imports

--- pages/propiedades/index.tsx
+++ pages/propiedades/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropertyList from "@/components/buscadorDePropiedades/PropertyList";
+
 const Index = () => {
-  const handleSubmit = (formData: any) => {};
   return (
     <>
       <PropertyList />
